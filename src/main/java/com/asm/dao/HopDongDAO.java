605380ecbb5adb235175e9c20622f38161df6e8f
Kiệt Kùn <ilovevy19@gmail.com>
2024-02-23 12:42:37 +0700

    12:41 02/23/24

--- src/main/java/com/asm/dao/HopDongDAO.java
+++ src/main/java/com/asm/dao/HopDongDAO.java
@@ -1,9 +1,12 @@
 package com.asm.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.asm.entity.HopDong;
 
 public interface HopDongDAO extends JpaRepository<HopDong, Integer> {
 	
+	List<HopDong> findByNhanVienIsNull();
 }
